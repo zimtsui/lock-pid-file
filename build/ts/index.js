db84cc933445f76eea4c1278d3c5044dc7ddf7b6
zim <zimtsui@gmail.com>
2022-07-16 01:31:18 +0800

    deps

--- build/ts/index.js
+++ build/ts/index.js
@@ -10,20 +10,20 @@ const path_1 = require("path");
 const { O_WRONLY, O_CREAT } = fs_1.constants;
 const XDG_RUNTIME_DIR = process.env['XDG_RUNTIME_DIR'];
 assert(XDG_RUNTIME_DIR);
-assert(path_1.isAbsolute(XDG_RUNTIME_DIR));
+assert((0, path_1.isAbsolute)(XDG_RUNTIME_DIR));
 function lockPidFile(appName) {
     assert(identifier_cases_1.kebabCase.test(appName));
-    const pidFilePath = path_1.join(XDG_RUNTIME_DIR, `${appName}.pid`);
-    const fd = fs_1.openSync(pidFilePath, O_WRONLY | O_CREAT, 0o777);
-    fcntl_1.fcntl(fd, fcntl_1.F_SETLK, {
+    const pidFilePath = (0, path_1.join)(XDG_RUNTIME_DIR, `${appName}.pid`);
+    const fd = (0, fs_1.openSync)(pidFilePath, O_WRONLY | O_CREAT, 0o777);
+    (0, fcntl_1.fcntl)(fd, fcntl_1.F_SETLK, {
         l_type: fcntl_1.F_WRLCK,
         l_whence: fcntl_1.SEEK_SET,
         l_start: 0,
         l_len: 0,
         l_pid: 0,
     });
-    fs_1.ftruncateSync(fd, 0);
-    fs_1.writeSync(fd, Buffer.from(`${process.pid}\n`));
+    (0, fs_1.ftruncateSync)(fd, 0);
+    (0, fs_1.writeSync)(fd, Buffer.from(`${process.pid}\n`));
 }
 exports.lockPidFile = lockPidFile;
 exports.default = lockPidFile;
